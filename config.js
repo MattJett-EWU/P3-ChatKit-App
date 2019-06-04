804b726380e0b582b647bb404236782ef4ab6d70
Matthew Jett <matthew.jett@outlook.com>
2019-06-03 19:37:30 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/ebd30783-ebb3-4a04-8a72-dacb5fa3aaed/token";
+const instanceLocator = "v1:us1:ebd30783-ebb3-4a04-8a72-dacb5fa3aaed";
+
+export { tokenUrl, instanceLocator }
